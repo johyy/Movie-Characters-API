ac27b386721e425fc70b5d7058471b6b3e3e9ddc
Joni Kokko <joni.j.kokko@gmail.com>
2022-09-01 13:41:37 +0300

    Initial commit

---
+++ src/main/java/com/example/java_assignment_3/JavaAssignment3Application.java
@@ -0,0 +1,13 @@
+package com.example.java_assignment_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssignment3Application.class, args);
+	}
+
+}
